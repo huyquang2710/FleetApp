5421daf46d36e53554b732a64a305d52e4f49b5a
Huy Quang <huyquang271099@gmail.com>
2021-08-02 01:18:26 +0700

    Obtain and Set Up the Nice Admin Template. (exclude = { SecurityAutoConfiguration.class }).

--- src/main/java/com/hquang/AppSpring/AppSpringApplication.java
+++ src/main/java/com/hquang/AppSpring/AppSpringApplication.java
@@ -2,8 +2,9 @@ package com.hquang.AppSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class AppSpringApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hquang/AppSpring/ApplicationController.java
@@ -0,0 +1,12 @@
+package com.hquang.AppSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	@GetMapping("/index")
+	public String goHome() {
+		return "index";
+	}
+}
