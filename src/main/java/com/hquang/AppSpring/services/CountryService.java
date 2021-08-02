bd847bba907b13b7dd7d32b3f95bbad11fdfa614
Huy Quang <huyquang271099@gmail.com>
2021-08-03 04:12:44 +0700

    Insert a New Country Using Modal Form

--- src/main/java/com/hquang/AppSpring/services/CountryService.java
+++ src/main/java/com/hquang/AppSpring/services/CountryService.java
@@ -14,8 +14,14 @@ public class CountryService {
 	@Autowired
 	CountryRepository repository;
 	
+	//Return List add counties
 	public List<Country> getFindAllCountries() {
 		return repository.findAll();
+	}
+	
+	//Save a country
+	public void save(Country country) {
+		repository.save(country);
 	}
 
 }
