20829c987d58b113936f6dece23d6211ff21e156
Huy Quang <huyquang271099@gmail.com>
2021-08-04 22:05:16 +0700

    Implement Jpa Auditing

--- src/main/java/com/hquang/AppSpring/AppSpringApplication.java
+++ src/main/java/com/hquang/AppSpring/AppSpringApplication.java
@@ -2,10 +2,18 @@ package com.hquang.AppSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication /* (exclude = { SecurityAutoConfiguration.class }) */
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AppSpringApplication {
+	
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new SpringSecurityAuditorAware();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppSpringApplication.class, args);
